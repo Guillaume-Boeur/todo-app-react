9116e17c9f5a52a9911a64708f5f02493a25df33
Guillaume-Boeur <guillaume.boeur@gmail.com>
2020-05-14 12:01:44 +0200

    Pass fictive data api with props in Table

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -3,9 +3,28 @@ import Table from './Table'
 
 class App extends Component {
   render() {
+    // as data API
+    const characters = [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+    ]
     return (
       <div className="container">
-        <Table />
+        <Table characterData={characters} />
       </div>
     )
   }
