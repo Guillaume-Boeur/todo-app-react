db9aa5483f34716cd6210699e1d6f93a18103c67
Guillaume-Boeur <guillaume.boeur@gmail.com>
2020-05-14 16:35:44 +0200

    add Api.js file to test how to pulling in data from an API

---
+++ todo-app/src/Api.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+// You can switch between the app I created and this test file by just changing the URL in index.js - import App from './Api';.
+
+class App extends Component {
+  state = {
+    data: [],
+  }
+
+  // Code is invoked after the component is mounted/inserted into the DOM tree.
+  componentDidMount() {
+    const url =
+      'https://en.wikipedia.org/w/api.php?action=opensearch&search=Seona+Dancing&format=json&origin=*'
+
+    fetch(url)
+      .then(result => result.json())
+      .then(result => {
+        this.setState({
+          data: result,
+        })
+      })
+  }
+
+  render() {
+    const { data } = this.state
+
+    const result = data.map((entry, index) => {
+      return <li key={index}>{entry}</li>
+    })
+
+    return <ul>{result}</ul>
+  }
+}
+
+export default App
